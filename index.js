1902a32d3ca6154bb4964754ca3a771f86368e94
Mannyjung <614259041@webmail.npru.ac.th>
2021-10-23 18:26:45 +0700

    testAccessC1826

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const app = require('express')
-router.get('/', function(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true); 
-});
+// router.get('/', function(req, res) {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   res.setHeader('Access-Control-Allow-Credentials', true); 
+// });
 const port = process.env.PORT || 4000;
 const http = require('http').createServer(app)
 const cors = require("cors");
